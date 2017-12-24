a8b057cc84287d9b71b842e6efd5f989eddd90e1
豫风 <991810133@qq.com>
2017-12-24 10:55:44 +0800

    初始化code

--- HHModelGenerate/HHModel.h
+++ HHModelGenerate/HHModel.h
@@ -1,6 +1,6 @@
 //
 //  HHModel.h
-//  HHAmazing
+//  HHModelGenerate
 //
 //  Created by 豫风 on 2017/12/21.
 //  Copyright © 2017年 豫风. All rights reserved.
--- HHModelGenerate/test/Hello.m
+++ HHModelGenerate/test/Hello.m
@@ -1,6 +1,6 @@
 //
 //  hello.m
-//  HHAmazing
+//  HHModelGenerate
 //
 //  Created by 豫风 on 2017/12/22.
 //  Copyright © 2017年 豫风. All rights reserved.
